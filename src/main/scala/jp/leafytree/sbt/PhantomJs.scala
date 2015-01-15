ea00048afadea0ce90322514a0d1ea53a355ba59
Isamu Mogi <isamu@leafytree.jp>
2015-01-16 01:55:10 +0900

    Extract PhantomJsPlugin's code to subclass

--- src/main/scala/jp/leafytree/sbt/PhantomJs.scala
+++ src/main/scala/jp/leafytree/sbt/PhantomJs.scala
@@ -1,4 +1,41 @@
 package jp.leafytree.sbt
 
-object PhantomJs extends PhantomJsPlugin {
+import java.nio.charset.Charset
+import java.util.Properties
+
+import sbt._
+import Keys._
+
+import collection.JavaConversions._
+
+object PhantomJs extends AutoPlugin {
+  object autoImport {
+    lazy val installPhantomJs = taskKey[Unit]("Install PhantomJS")
+    lazy val checkInstalledPhantomJs = taskKey[Unit]("Check installed PhantomJS")
+  }
+
+  import autoImport._
+
+  override lazy val projectSettings = Seq(
+    installPhantomJs := {
+      val propertiesFile = PhantomJsInstaller.install(baseDirectory.value / "target")
+      val properties = new Properties()
+      IO.reader(propertiesFile, Charset.forName("UTF-8")){ properties.load(_) }
+      properties.foreach { case (key, value) => {
+        System.setProperty(key, value)
+        javaOptions += f"-D${key}=${value}" // TODO: empty character?
+      }}
+    },
+
+    checkInstalledPhantomJs := {
+      val propertiesFile = baseDirectory.value / "target" / "phantomjs.properties"
+      val properties = new Properties()
+      IO.reader(propertiesFile, Charset.forName("UTF-8")){ properties.load(_) }
+      val command = Array(properties.getProperty("phantomjs.binary.path"), "--version")
+      val result = scala.sys.process.Process(command).run.exitValue
+      if (result != 0) {
+        sys.error("PhantomJS execution failure")
+      }
+    }
+  )
 }
--- src/main/scala/jp/leafytree/sbt/SbtPhantomJs.scala
+++ src/main/scala/jp/leafytree/sbt/SbtPhantomJs.scala
@@ -1,5 +1,15 @@
 package jp.leafytree.sbt
 
+import java.nio.charset.Charset
+import java.util.Properties
+
+import sbt._
+import Keys._
+
+import collection.JavaConversions._
+
 @deprecated("Please use `PhantomJs`")
-object SbtPhantomJs {
+object SbtPhantomJs extends AutoPlugin {
+  override def requires = PhantomJs
+  override def trigger = allRequirements
 }
