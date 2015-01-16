da2f6a6b84cdb3d998e5642d36413464fbbd3e69
Isamu Mogi <isamu@leafytree.jp>
2015-01-16 22:31:37 +0900

    jiji

--- src/sbt-test/setup/specs2/src/test/scala/HelloSpec.scala
+++ src/sbt-test/setup/specs2/src/test/scala/HelloSpec.scala
@@ -2,8 +2,8 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.specs2.mutable._
 
 class HelloSpec extends Specification {
-  "The 'Hello world' string" should {
-    "contain 11 characters" in {
+  "PhantomJSDriver" should {
+    "get web content" in {
       val driver = new PhantomJSDriver()
       try {
         driver.get("http://saturday06.github.io/sbt-phantomjs/test/hello.html")
