4e49cecfd32197e41bbb2748f72304aa8029b00d
Isamu Mogi <isamu@leafytree.jp>
2015-01-16 22:55:29 +0900

    Use PhantomJSDriver

--- sample/simple/src/main/scala/Main.scala
+++ sample/simple/src/main/scala/Main.scala
@@ -1,6 +1,13 @@
+import org.openqa.selenium.phantomjs.PhantomJSDriver
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val k = "phantomjs.binary.path"
-    println(k + ": " + System.getProperty(k))
+    val driver = new PhantomJSDriver()
+    try {
+      driver.get("http://saturday06.github.io/sbt-phantomjs/test/hello.html")
+      assert(driver.getTitle() == "Hello, PhantomJS!")
+    } finally {
+      driver.quit()
+    }
   }
 }
